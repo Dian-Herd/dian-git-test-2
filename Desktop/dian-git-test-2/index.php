aaca45e257a8a9ab95889a101b7b4515bd9749e4
Dian-Herd <dian92943@gmail.com>
2021-01-02 03:21:37 +0700

    menambahkan paragraf

--- Desktop/dian-git-test-2/index.php
+++ Desktop/dian-git-test-2/index.php
@@ -7,5 +7,6 @@
 </head>
 <body>
     <h1>Hello World!</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt delectus voluptatibus, veritatis sit laboriosam blanditiis qui adipisci? Aperiam distinctio consectetur consequatur suscipit veniam, ipsa fugit quo, pariatur corrupti ab at!</p>
 </body>
 </html>
